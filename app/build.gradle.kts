d3a3444185f5763fec1593e78b0c1514fe511ee7
Prakash Nayak <prakashnayakp9686@gmail.com>
2026-06-21 11:21:11 +0530

    Added viewmodel dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.navigation.compose)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
